e8f623919eecf0470477628d50809dac56be8a69
nullnag <153712889+nullnag@users.noreply.github.com>
2024-08-16 00:30:52 +0300

    Add files via upload

---
+++ Client/Client.cpp
@@ -0,0 +1,170 @@
+﻿// Client.cpp: определяет точку входа для приложения.
+//
+
+#include "Client.h"
+#include <iostream>
+#include <winsock2.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+#define PORT 8080
+
+
+int main()
+{
+	WSADATA wsaData;
+	SOCKET sock = INVALID_SOCKET;
+	struct sockaddr_in serverAddr;
+	char buffer[1024] = { 0 };
+	char message[1024] = {0};
+	int result;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		std::cerr << "Error initialize WinSock\n";
+		return 1;
+	}
+
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET) {
+		std::cerr << "Error create  socket\n";
+		WSACleanup();
+		return 1;
+	}
+
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serverAddr.sin_port = htons(PORT);
+
+	if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+		std::cerr << "Error connect to server\n";
+		closesocket(sock);
+		WSACleanup();
+		return 1;
+	}
+
+	char option = ' ';
+	std::string username;
+	std::string password;
+	std::string response;
+	bool pass = false;
+
+	while (true) {
+		if (pass == true) {
+			break;
+		}
+		std::cout << "Registy - R\n";
+		std::cout << "Log In - L\n";
+		std::cout << "Choose option: ";
+		std::cin >> option;
+		switch (option)
+		{
+		case 'R':
+			system("cls");
+			std::cout << "Exit - E\n";
+			while (true) {
+				std::cout << "Choose your username: ";
+				std::cin >> username;
+				if (username == "E") {
+					break;
+				}
+				if (username.length() > 6 && username.length() < 16) {
+					break;
+				}
+				std::cout << "ERROR: Invalid Login\n";
+			}
+			if (username == "E") {
+				break;
+			}
+			while (true) {
+				std::cout << "\nChoose your password: ";
+				std::cin >> password;
+				if (password.length() > 8) {
+					break;
+				}
+				std::cout << "password less than 8 symbols\n";
+			}
+			response = "R|" + username + "|" + password + "|";
+			std::strcpy(message, response.c_str());
+			send(sock, message, strlen(message), 0);
+			result = recv(sock, buffer, sizeof(buffer), 0);
+			if (buffer[0] == 'U') {
+				std::cout << "ERROR: User exists\n";
+				continue;
+			}
+			else {
+				std::cout << "Success\n";
+				continue;
+			}
+		case 'L':
+			system("cls");
+			std::cout << "Exit - E\n";
+			while (true) {
+				std::cout << "Username: ";
+				std::cin >> username;
+				if (username == "E") {
+					break;
+				}
+				std::cout << "Password: ";
+				std::cin >> password;
+				response = "L|" + username + "|" + password + "|";
+				std::strcpy(message, response.c_str());
+				send(sock, message, strlen(message), 0);
+				result = recv(sock, buffer, sizeof(buffer), 0);
+				if (buffer[0] == 'S') {
+					std::cout << "Success\n";
+					pass = true;
+					break;
+				}
+				else {
+					std::cout << "ERROR: Invalid Login or password\n";
+					continue;
+				}
+				break;
+			}
+		default:
+			std::cout << "Wrong option.\n";
+			continue;
+		}
+	}
+	system("cls");
+	std::string userToChat;
+	std::cout << "Choose option: \n";
+	std::cout << "Chose user to chat - C\n";
+	std::cout << "Exit - E\n";
+	while (true) {
+		std::cin >> option;
+		if (option == 'E') {
+			break;
+		}
+		switch (option)
+		{
+		case 'C':
+			while (true) {
+				std::cout << "Exit - E\n";
+				std::cout << "Search user to chat: ";
+				std::cin >> userToChat;
+				if (userToChat == "E") {
+					break;
+				}
+				response = "C|" + userToChat + "|";
+				std::strcpy(message, response.c_str());
+				send(sock, message, strlen(message), 0);
+				result = recv(sock, buffer, sizeof(buffer), 0);
+				if (buffer[0] == 'S') {
+					std::cout << "Chat: \n";
+					while (true) {
+						
+					}
+				}
+				else {
+					std::cout << "ERROR: User not found\n";
+					continue;
+				}
+			}
+		default:
+			break;
+		}
+	}
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,199 @@
+﻿
+#include "Server.h"
+#include <iostream>
+#include <winsock2.h>
+#include <cstring>
+#include <thread>
+#include <sqlite3.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+#define PORT 8080
+sqlite3* db;
+sqlite3_stmt* stmt;
+
+void initializeDataBase(sqlite3* &db) {
+	int rc = sqlite3_open("users.db", &db);
+	char* errMsg = nullptr;
+	if (rc) {
+		std::cerr << "Can't open database: " << sqlite3_errmsg(db);
+		return;
+	}
+	else {
+		std::cout << "Success open database\n";
+	}
+	const char* createTableSql = "CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT);";
+	rc = sqlite3_exec(db, createTableSql, nullptr,0,&errMsg);
+	if (rc != SQLITE_OK) {
+		std::cerr << "SQL error:" << errMsg << "\n";
+		sqlite3_free(errMsg);
+	}
+}
+
+bool userExists(sqlite3*& db, std::string& username, SOCKET clientSock, sqlite3_stmt*& stmt) {
+	const char* sql = "SELECT 1 FROM users WHERE username = ?;";
+	if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) != SQLITE_OK) {
+		std::cerr << "Error preparing statement: " << sqlite3_errmsg(db) << std::endl;
+		return true;
+	}
+	sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
+	if (sqlite3_step(stmt) == SQLITE_ROW) {
+		return true;
+	}
+	sqlite3_finalize(stmt);
+	return false;
+}
+
+void addUser(sqlite3* &db, std::string& username, std::string& password, SOCKET& clientSock) {
+	const char* response = "S";
+	if (userExists(db, username, clientSock,  stmt)) {
+		response = "U";
+		send(clientSock, response, 1, 0);
+		return;
+	}
+	const char* sql = "INSERT INTO users (username, password) VALUES (?, ?);";
+	if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) != SQLITE_OK) {
+		std::cerr << "Error preparing statement: " << sqlite3_errmsg(db) << std::endl;
+		return;
+	}
+	sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 2, password.c_str(), -1, SQLITE_STATIC);
+	if (sqlite3_step(stmt) != SQLITE_DONE) {
+		std::cerr << "Error executing statement: " << sqlite3_errmsg(db) << std::endl;
+		sqlite3_finalize(stmt);
+		return;
+	}
+	send(clientSock, response, strlen(response), 0);
+	sqlite3_finalize(stmt);
+}
+
+
+void convertToString(std::string& option, char buffer[1024], int& itter) {
+	for (; itter < strlen(buffer) ; itter++) {
+		if (buffer[itter] == '|') {
+			itter++;
+			break;
+		}
+		option += buffer[itter];
+	}
+}
+
+void checkLoginPassword(std::string& username, std::string& password, SOCKET& clientSock) {
+	sqlite3_stmt* stmt;
+
+	const char* sql = "SELECT 1 FROM users WHERE username = ? AND password = ?;";
+	if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) != SQLITE_OK) {
+		std::cerr << "Error preparing: " << sqlite3_errmsg(db) << "\n";
+		return;
+	}
+	sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 2, password.c_str(), -1, SQLITE_STATIC);
+	if (sqlite3_step(stmt) == SQLITE_ROW) {
+		send(clientSock, "S", 1, 0);
+	}
+	else {
+		send(clientSock, "U", 1, 0);
+	}
+	sqlite3_finalize(stmt);
+	return;
+}
+
+void handleClient(SOCKET clientSock) {
+	char buffer[1024] = {0};
+	while (true) {
+		int itter = 2;
+		int result = recv(clientSock, buffer, sizeof(buffer) - 1, 0);
+		if (result > 0) {
+			std::string username;
+			std::string password;
+			// R - Registy // L - LogIn // C - Chose user to chat
+			switch (buffer[0]) 
+			{
+			case 'R':
+				convertToString(username, buffer,itter);
+				convertToString(password, buffer,itter);
+				addUser(db,username,password, clientSock);
+				break;
+			case 'L':
+				convertToString(username, buffer,itter);
+				convertToString(password, buffer, itter);
+				checkLoginPassword(username, password, clientSock);
+				break;
+			case 'C':
+				convertToString(username, buffer, itter);
+				const char* response = "S";
+				if (userExists(db, username, clientSock, stmt)) {
+				}
+				else {
+					response = "U";
+				}
+				send(clientSock, response, 1, 0);
+				break;
+			}
+			std::cout << "Message from client: " << buffer << "\n";
+			const char* response = "Hello from server";
+		}
+		else if (result == 0) {
+			std::cout << "Connection closed from client\n";
+			break;
+		}
+		else {
+			std::cerr << "recv failed with error: " << WSAGetLastError() << std::endl;
+			break;
+		}
+	}
+	closesocket(clientSock);
+}
+
+int main()
+{
+	WSADATA wsaData;
+	SOCKET serverSock = INVALID_SOCKET;
+	SOCKET clientSock = INVALID_SOCKET;
+	struct sockaddr_in serverAddr, clientAddr;
+	int clientAddrSize = sizeof(clientAddr);
+
+	initializeDataBase(db);
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		std::cerr << "Error WinSock \n";
+		return 1;
+	}
+	serverSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (serverSock == INVALID_SOCKET) {
+		std::cerr << "Error sock creating \n";
+		WSACleanup();
+		return 1;
+	}
+
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+	serverAddr.sin_port = htons(PORT);
+
+	if (bind(serverSock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+		std::cerr << "Error bind sockets\n";
+		closesocket(serverSock);
+		WSACleanup();
+		return 1;
+	}
+
+	if (listen(serverSock, 1) == SOCKET_ERROR) {
+		std::cerr << "Error to listen";
+		closesocket(serverSock);
+		WSACleanup();
+		return 1;
+	}
+
+	std::cout << "Waiting for connetion\n";
+	while (true) {
+		clientSock = accept(serverSock, (struct sockaddr*)&clientAddr, &clientAddrSize);
+		if (clientSock == INVALID_SOCKET) {
+			std::cerr << "Error accept connection\n";
+			continue;
+		}
+		std::thread(handleClient, clientSock).detach();
+	}
+	closesocket(clientSock);
+	closesocket(serverSock);
+	WSACleanup();
+	return 0;
+}
